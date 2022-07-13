b04f72e4789fe6a6fc95bedbb0ff23f1d8efd1c6
Vasyl Bondarchuk <teorfiz@gmail.com>
2022-07-13 22:41:54 +0300

    Add MassAction change quote status to Declined

---
+++ Controller/Adminhtml/Index/MassStatus.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace CustomerQuote\CustomerQuoteAdminUi\Controller\Adminhtml\Index;
+
+use Magento\Backend\Model\View\Result\Redirect;
+use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Ui\Component\MassAction\Filter;
+use Psr\Log\LoggerInterface;
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+
+use CustomerQuote\CustomerQuoteAdminUi\Api\Data\Quote\QuoteRepositoryInterface;
+use CustomerQuote\CustomerQuoteAdminUi\Model\ResourceModel\Quote\CollectionFactory;
+
+
+class MassStatus extends Action implements HttpPostActionInterface
+{
+    /**
+     * Massactions filter
+     *
+     * @var Filter
+     */
+    protected Filter $filter;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected CollectionFactory $collectionFactory;
+
+
+    private QuoteRepositoryInterface $quoteRepository;
+
+    /**
+     * @var LoggerInterface
+     */
+    private ?LoggerInterface $logger;
+
+    public function __construct(
+        Context                   $context,
+        Filter                    $filter,
+        CollectionFactory         $collectionFactory,
+        QuoteRepositoryInterface  $quoteRepository,
+        LoggerInterface           $logger = null
+    ) {
+        parent::__construct($context);
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+        $this->quoteRepository = $quoteRepository;
+        $this->logger = $logger;
+    }
+
+    /**
+     * Mass Delete Action
+     *
+     * @return Redirect
+     * @throws LocalizedException
+     */
+
+
+    public function execute()
+    {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        $quoteStatus = 0;
+        $quoteStatusError = 0;
+        $status = $this->getRequest()->getParam('status');
+
+        foreach ($collection->getItems() as $quote) {
+            try {
+                $this->quoteRepository->save($quote->setQuoteStatus($status));
+                $quoteStatus++;
+            } catch (LocalizedException $exception) {
+                $this->logger->error($exception->getLogMessage());
+                $quoteStatusError++;
+            }
+        }
+
+        if ($quoteStatus) {
+            $this->messageManager->addSuccessMessage(
+                __('A total of %1 record(s) have been updated.', $quoteStatus)
+            );
+        }
+
+        if ($quoteStatusError) {
+            $this->messageManager->addErrorMessage(
+                __(
+                    'A total of %1 record(s) haven\'t been updated. Please see server logs for more details.',
+                    $quoteStatusError
+                )
+            );
+        }
+
+        return $this->resultFactory->create(ResultFactory::TYPE_REDIRECT)->setPath('quote/index/index/');
+    }
+}
---
+++ Model/Source/QuoteStatus.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace CustomerQuote\CustomerQuoteAdminUi\Model\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+use CustomerQuote\CustomerQuoteAdminUi\Model\UpdateQuote;
+
+class QuoteStatus implements ArrayInterface
+{
+    /**
+     * Get options
+     *
+     * @return array
+     */
+
+    const OPTION_VALUES = [
+        UpdateQuote::QUOTE_STATUS_NEW,
+        UpdateQuote::QUOTE_STATUS_OPEN,
+        UpdateQuote::QUOTE_STATUS_SUBMITTED,
+        UpdateQuote::QUOTE_STATUS_UPDATED,
+        UpdateQuote::QUOTE_STATUS_CLIENT_REVIEWED,
+        UpdateQuote::QUOTE_STATUS_ORDERED,
+        UpdateQuote::QUOTE_STATUS_CLOSED,
+        UpdateQuote::QUOTE_STATUS_DECLINED
+    ];
+    const OPTION_LABELS = self::OPTION_VALUES;
+
+    /**
+     * @return array
+     */
+    public function toOptionArray(): array
+    {
+        $toOptionArray = [];
+        foreach($this->getOptionValuesLabelsArray() as $value => $label){
+            $toOptionArray[] = [
+                'label' => __($label),
+                'value' => $value
+            ];
+        }
+        return $toOptionArray;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptionValuesLabelsArray() : array
+    {
+        return array_combine(self::OPTION_VALUES, self:: OPTION_LABELS);
+    }
+}
+
